7158146011b22ea90f85a5ba75c75ae8d12ba08d
Yuli-M <yuli.star.194@gmail.com>
2024-09-29 21:35:49 -0700

    lib

---
+++ lib/screens/inicio.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Inicio extends StatelessWidget {
+  const Inicio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
